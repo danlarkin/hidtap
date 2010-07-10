230e4904ae4952e934a40d9b567c25d194f273c2
Dan Larkin <dan@danlarkin.org>
2010-07-10 00:56:02 -0400

    print diagnostic info on startup

--- src/hidtap.m
+++ src/hidtap.m
@@ -1,6 +1,22 @@
 #import "Tap.h"
+#import "Config.h"
 
 int main (int argc, const char * argv[]) {
+  NSArray *modifiers = [Config getModifiers];
+
+  if ([modifiers count] == 0) {
+    NSLog(@"No Modifiers Configured, exiting.");
+    exit(0);
+  }
+
+  NSLog(@"Configured Modifiers:");
+  for(NSDictionary *modifier in [Config getModifiers]) {
+    NSLog(@"Keyboard: %@\tModifier: %@\tKeycode: %@",
+          [modifier valueForKey:@"Keyboard"],
+          [modifier valueForKey:@"Modifier"],
+          [modifier valueForKey:@"Keycode"]);
+  }
+
   NSRunLoop *loop = [NSRunLoop currentRunLoop];
   Tap *tap = [Tap tapOnRunLoop:loop];
 
